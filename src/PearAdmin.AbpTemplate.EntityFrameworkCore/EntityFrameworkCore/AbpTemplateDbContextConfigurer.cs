6cae510017bd439b969cd71f6b10717232782228
sassassin <530521314@qq.com>
2021-06-16 20:47:34 +0800

    升级到.net 5.0

--- src/PearAdmin.AbpTemplate.EntityFrameworkCore/EntityFrameworkCore/AbpTemplateDbContextConfigurer.cs
+++ src/PearAdmin.AbpTemplate.EntityFrameworkCore/EntityFrameworkCore/AbpTemplateDbContextConfigurer.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Common;
+﻿using System;
+using System.Data.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace PearAdmin.AbpTemplate.EntityFrameworkCore
@@ -7,12 +8,12 @@ namespace PearAdmin.AbpTemplate.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<AbpTemplateDbContext> builder, string connectionString)
         {
-            builder.UseMySql(connectionString);
+            builder.UseMySql(connectionString, new MySqlServerVersion(new Version(5, 7)));
         }
 
         public static void Configure(DbContextOptionsBuilder<AbpTemplateDbContext> builder, DbConnection connection)
         {
-            builder.UseMySql(connection);
+            builder.UseMySql(connection, new MySqlServerVersion(new Version(5, 7)));
         }
     }
 }
